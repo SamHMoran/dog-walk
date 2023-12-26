494dc2a9dee0f2b926ccc8c3446278c779ab6eb2
SamHMoran <samholmesmoran@gmail.com>
2023-12-26 17:36:11 -0500

    Project Initialization and Setup

--- README.md
+++ README.md
@@ -1,2 +1,19 @@
 # dog-walk
+
+## Description
 Dog walk simulator
+
+## Installation
+
+1. Clone the repository: `git clone https://github.com/SamHMoran/dog-walk.git`
+2. Navigate to the project directory: `cd dog-walk`
+3. Install the required dependencies: `npm install`
+
+## Usage
+
+## Contributing
+
+## License
+
+Token: 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
+
--- game.js
+++ game.js
@@ -1,14 +1,45 @@
-  const canvas = document.getElementById("gameCanvas");
-  const ctx = canvas.getContext("2d");
+// Import Phaser
+import * as Phaser from 'phaser';
 
-  function gameLoop() {
-    // Update game state
+// Define your game scenes
+function preload() {
+  // Preload assets here
+}
 
-    // Draw game state
+function create() {
+  // Create game objects here
+  let sprite = this.physics.add.sprite(100, 450, 'mySprite');
+}
 
-    // Request next frame
-    requestAnimationFrame(gameLoop);
+// Configure game settings
+const config = {
+  type: Phaser.AUTO, // Use Phaser's automatic rendering mode
+  width: 800, // Set the width of the game
+  height: 600, // Set the height of the game
+  parent: 'gameCanvas', // The ID of the DOM element to append the game to
+  physics: {
+      default: 'arcade',
+      arcade: {
+          gravity: { y: 200 }
+      }
+  },
+  scene: {
+      preload: preload,
+      create: create
   }
+};
 
-  // Start the game loop
-  gameLoop();
+// Initialize Phaser game
+const game = new Phaser.Game(config);
+
+// Start the game loop
+gameLoop();
+
+function gameLoop() {
+  // Update game state
+
+  // Draw game state
+
+  // Request next frame
+  requestAnimationFrame(gameLoop);
+}
